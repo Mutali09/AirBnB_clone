f9e71bebfa33c6ae3e8a60622f82416bd2b5f4bc
Mutali09 <nelsonsimiyu9@gmail.com>
2024-01-10 20:56:44 -0800

    models

--- models/base_model.py
+++ models/base_model.py
@@ -27,3 +27,31 @@ class BaseModel:
                     self.__dict__[k] = v
         else:
             models.storage.new(self)
+
+    def save(self):
+        """Update the last modification time, which is updated_at, and save object to storage."""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """Return a dictionary representation ofBaseModel instance.
+
+        Includes the key/value pairs for all attributes, with special
+        handling for created_at and updated_at attritbutes, which are converted
+        to ISO format strings. Includes key/value pair for __class__ representing
+        class name of object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the string representation of BaseModel instance.
+
+        Displays the class name, ID, and key/value pairs of
+        attributes in a formatted string.
+        """
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
